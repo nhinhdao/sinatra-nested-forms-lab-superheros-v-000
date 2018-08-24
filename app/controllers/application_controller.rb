4143ad8c7d015fd6e8529b6ebe312b68b18806bf
Nhinh Dao <nhinhdao.vn@gmail.com>
2018-08-24 02:29:29 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,13 +1,13 @@
 require 'sinatra/base'
 
 class App < Sinatra::Base
-   
+
    set :views, Proc.new { File.join(root, "../views/") }
-   
+
    get '/' do
       erb :super_hero
    end
-   
+
    post '/teams' do
       @team = Superhero.new(params[:team])
 
