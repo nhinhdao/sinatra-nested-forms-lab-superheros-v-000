a0ca7b9bf0b766dd07e4e843b05a64cbf7886261
Nhinh Dao <nhinhdao.vn@gmail.com>
2018-08-24 02:28:43 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,18 @@
 require 'sinatra/base'
 
 class App < Sinatra::Base
+   
+   set :views, Proc.new { File.join(root, "../views/") }
+   
+   get '/' do
+      erb :super_hero
+   end
+   
+   post '/teams' do
+      @team = Superhero.new(params[:team])
 
-    set :views, Proc.new { File.join(root, "../views/") }
-
-    get '/' do
-       erb :super_hero
-    end
+      params[:hero].each { |hero|  Superhero.new(hero)}
+      @hero = Superhero.all
+      erb :team
+   end
 end
